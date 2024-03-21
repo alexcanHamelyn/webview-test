bfa07b08c19c74ed85ede5350d1b762e32ae337c
Alex Cantón <107461147+alexcanHamelyn@users.noreply.github.com>
2024-03-21 14:53:10 +0100

    Add eventListener

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
 "use client"
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [type, setType] = useState('');
+  const [data, setData] = useState('');
   useEffect(() => {
     console.log("Init");
     window.addEventListener('message', (event) => {
       const { type, data } = event.data;
 
+      setType(type);
+      setData(data);
       console.log(type);
       console.log(data);
       // Aquí puedes manejar el tipo de mensaje y los datos recibidos
@@ -20,6 +24,10 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-5xl">Hamelyn Test</h1>
+      <hr />
+      <pre><code>{JSON.stringify(type, null, 2)}</code></pre>
+      <hr />
+      <pre><code>{JSON.stringify(data, null, 2)}</code></pre>
     </main>
   );
 }
