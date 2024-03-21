348539efe651d7587129cddc7064b6841b258530
MarcosTerroso <107342818+MarcosTerroso@users.noreply.github.com>
2024-03-21 15:21:02 +0100

    add user

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,29 +5,23 @@ import { useEffect, useState } from "react";
 export default function Home() {
   const [type, setType] = useState('');
   const [data, setData] = useState('');
+  const [user, setUser] = useState('');
   useEffect(() => {
-    console.log("Init");
     window.addEventListener('message', (event) => {
-      const { type, data } = event.data;
+      const { user } = event.data;
 
-      setType(type);
-      setData(data);
-      console.log(type);
-      console.log(data);
+      // setType(type);
+      // setData(data);
+      setUser(user);
       // Aquí puedes manejar el tipo de mensaje y los datos recibidos
-      if (type === 'miTipoDeMensaje') {
-        console.log('Datos recibidos:', data);
-        // Haz lo que necesites con los datos recibidos
-      }
+
     });
   }, []);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-5xl">Hamelyn Test</h1>
       <hr />
-      <pre><code>{JSON.stringify(type, null, 2)}</code></pre>
-      <hr />
-      <pre><code>{JSON.stringify(data, null, 2)}</code></pre>
+      <pre><code>{JSON.stringify(user, null, 2)}</code></pre>
     </main>
   );
 }
