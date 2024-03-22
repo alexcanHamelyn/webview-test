77fa429dbbfee7fed19c5c346c0606d1a6ddffd2
Alex Cantón <107461147+alexcanHamelyn@users.noreply.github.com>
2024-03-22 11:22:31 +0100

    Add axios to call api

---
+++ src/app/api/product/route.ts
@@ -0,0 +1,38 @@
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request) {
+    const customHeader = request.headers.get("H-shop-token");
+
+    if (customHeader === "DEFAULT_VALUE") {
+        return NextResponse.json({
+            "_id": "9788499087122",
+            "productType": "book",
+            "categories": [
+                "Literatura y ficción",
+                "Literatura mundial"
+            ],
+            "title": "El Monje Que Vendió Su Ferrari: Una Fábula Espiritual",
+            "description": "El monje que vendió su Ferrari es una fábula espiritual que, desde hace más de quince años, ha marcado la vida de millones de personas en todo el mundo.A través de sus páginas, conocemos la extraordinaria historia de Julian Mantle, un abogado de éxito que, tras sufrir un ataque al corazón, debe afrontar el gran vacío de su existencia. Inmerso en esta crisis existencial, Julian toma la radical decisión de vender todas sus pertenencias y viajar a la India. Es en un monasterio del Himalaya donde aprende las sabias y profundas lecciones de los monjes sobre la felicidad, el coraje, el equilibrio y la paz interior.Con esta historia tan especial e inolvidable, Robin Sharma nos enseña, paso a paso, una nueva manera de enfocar la vida personal, profesional y familiar. Nos muestra lo importante que es emprender un recorrido vital con una dirección clara, con pasión y armonía interior.*** Más de 5 millones de libros vendidos.Reseña:«Los libros de Robin Sharma ayudan a personas de todo el mundo a vivir una vida extraordinaria.»Paulo Coelho, autor de El alquimista ",
+            "image": "https://ul2efuvs5p2hmdcb.public.blob.vercel-storage.com/9788499087122.jpg",
+            "lastUpdated": "2023-10-24T10:40:28.356Z",
+            "asin": "8499087124",
+            "priceBuy": 0.7,
+            "priceSell": 7.058000000000002,
+            "lastPriceUpdate": "2024-03-16T09:38:05.664Z",
+            "needUpdate": false,
+            "attributes": {
+                "authors": [
+                    "Robin Sharma"
+                ],
+                "publisher": "Debolsillo; 001 edición (29 octubre 2010)",
+                "language": "ES",
+                "pages": 224,
+                "format": "Libro de bolsillo",
+                "weight": 154,
+                "releaseYear": 2010
+            }
+        }, { status: 200 });
+    }
+
+    return NextResponse.json({ message: "Error!" }, { status: 500 });
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,49 @@
 "use client"
 
+import axios from "axios";
 import { useEffect, useState } from "react";
 
 export default function Home() {
   const [type, setType] = useState('');
-  const [data, setData] = useState('');
+  const [data, setData] = useState('DEFAULT_VALUE');
+  const [result, setResult] = useState('');
   useEffect(() => {
-    console.log("Init");
     window.addEventListener('message', (event) => {
       const { type, data } = event.data;
 
-      setType(type);
-      setData(data);
-      console.log(type);
-      console.log(data);
-      // Aquí puedes manejar el tipo de mensaje y los datos recibidos
-      if (type === 'miTipoDeMensaje') {
-        console.log('Datos recibidos:', data);
-        // Haz lo que necesites con los datos recibidos
+      if (type && data) {
+        setType(type);
+        setData(data);
       }
     });
   }, []);
+
+  const handleClick = async () => {
+    try {
+      axios.defaults.headers.common["H-shop-token"] = data;
+      // 9788499087122
+      const res = await axios.get("https://webview-test-kappa.vercel.app/api/product");
+      setResult(res.data);
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        alert(error.message);
+      } else {
+        console.log(error)
+        alert("Unexpected error");
+      }
+    }
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-start p-24">
       <h1 className="text-5xl">Hamelyn Test</h1>
       <hr />
-      <pre><code>{JSON.stringify(type, null, 2)}</code></pre>
-      <hr />
-      <pre><code>{JSON.stringify(data, null, 2)}</code></pre>
+      <div className="mt-10">
+        <button onClick={handleClick} className="px-3 py-1 bg-green-600 rounded-full text-lg">Get 9788499087122 Product</button>
+      </div>
+      <div className="w-full flex flex-row justify-center items-center">
+        <div className="w-1/2"><pre><code>{JSON.stringify(result, null, 2)}</code></pre></div>
+      </div>
     </main>
   );
 }
